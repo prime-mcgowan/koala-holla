5b33536f75be4d0f3468425fd3ef901e71548eda
Laura McGowan <prime.lmcg@gmail.com>
2022-12-15 19:03:16 -0600

    table is rendering / ready to transfer $ delete button present on page

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,6 +15,7 @@ function setupClickListeners() {
     // get user input and put in an object
     // NOT WORKING YET :(
     // using a test object
+
     let koalaToSend = {
       name: 'testName',
       age: 'testName',
@@ -30,7 +31,27 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    type: 'GET',
+    url: '/koalas'
+  }).then(function(response) {
+    //render koalas
+    $('#viewKoalas').empty();
+
+    for (let koala of response) {
+      $('#viewKoalas').append(`
+        <tr>
+          <td>${koala.name}</td>
+          <td>${koala.age}</td>
+          <td>${koala.gender}</td>
+          <td>${koala.ready_to_transfer}</td>
+          <td>${koala.notes}</td>
+          <td><button>Ready for Transfer</button>
+          <td><button>Delete</button>
+        </tr>
+      `) // where does data-id=${koala.id?} belong???
+    }
+  })
 } // end getKoalas
 
 function saveKoala( newKoala ){
